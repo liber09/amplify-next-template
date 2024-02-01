1517bfbcc444c146c05a41670b811e0b81d8d396
liber09 <linda.bergsangel@hotmail.com>
2024-02-01 15:55:10 +0100

    Wrong link to ask ellie in footer

--- src/_components/footer/footer.tsx
+++ src/_components/footer/footer.tsx
@@ -13,7 +13,7 @@ export default function Footer() {
                     <Link className={styles.footerLink} href={'/about'}>Om oss</Link>
                     <Link className={styles.footerLink} href={'/contact'}>Kontakt</Link>
                     <Link className={styles.footerLink} href={'/services'}>E-tjänster</Link>
-                    <Link className={styles.footerLink} href={'/https://ellie-9694e4.zapier.app'}>Fråga Ellie</Link>
+                    <Link className={styles.footerLink} href={'https://ellie-9694e4.zapier.app/'}>Fråga Ellie</Link>
                     <Link className={styles.footerLink} href={'/about'}>Vårdcentraler</Link>
                     <Link className={styles.footerLink} href={'/about'}>Stöd & hjälp</Link>
                     <Link className={styles.footerLink} href={'/about'}>Föreningar</Link>
